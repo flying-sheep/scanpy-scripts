10976ed32ce19e1f7b99c83b61eaaebc013d901e
Philipp A <flying-sheep@web.de>
2018-11-12 10:05:31 +0100

    Converted to pip package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+script_files = (HERE / 'scanpy_cli').glob('scanpy_*.py')
+scripts = [
+    f'{file.with_suffix("").name.replace("_", "-")}=scanpy_cli.{file.name}:main'
+    for file in script_files
+]
+
+setup(
+    name='scanpy-cli',
+    version='1.0',
+    description='Scripts for using scanpy from the command line',
+    author='nh3',
+    #author_email='',
+    url='https://github.com/ebi-gene-expression-group/scanpy-scripts',
+    packages=['scanpy_cli'],
+    scripts=[
+        'scanpy-cli-tests.sh',
+        'scanpy-cli-tests.bats',
+    ],
+    console_scripts=scripts,
+)
